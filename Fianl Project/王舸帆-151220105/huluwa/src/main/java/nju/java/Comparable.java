c3deb4a5c4f44b3c05011efde5d529e07555d9ef
wwwwww <macbook@192.168.191.5>
2018-01-08 17:26:13 +0800

    del

--- Fianl Project/王舸帆-151220105/huluwa/src/main/java/nju/java/Comparable.java
+++
@@ -1,8 +0,0 @@
-package nju.java;
-
-/**
- * Created by macbook on 2017/10/16.
- */
-public interface Comparable {
-    boolean biggerThan(Comparable another);
-}
--- Fianl Project/王舸帆-151220105/huluwa/src/main/java/nju/java/Sorter.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by macbook on 2017/10/17.
- */
-package nju.java;
-
-public interface Sorter {
-    public void sort(Tspace tspace, Renwu renwus[], int h, int l);
-}
